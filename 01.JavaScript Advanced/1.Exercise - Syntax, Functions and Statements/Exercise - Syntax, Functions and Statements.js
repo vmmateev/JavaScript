b49d739fc8045859eb0315563390e52179a1ee48
vmmateev <v.m.mateev@gmail.com>
2021-09-15 20:06:24 +0300

    Homework

--- 01.JavaScript Advanced/1.Exercise - Syntax, Functions and Statements/Exercise - Syntax, Functions and Statements.js
+++ 01.JavaScript Advanced/1.Exercise - Syntax, Functions and Statements/Exercise - Syntax, Functions and Statements.js
@@ -4,8 +4,8 @@ function calcFruit(fruit, weight, pricePerKilo) {
     let totalPrice = weightInKilo * pricePerKilo;
     console.log(`I need $${totalPrice.toFixed(2)} to buy ${weightInKilo.toFixed(2)} kilograms ${fruit}.`);
 }
-calcFruit('orange', 2500, 1.80);
-calcFruit('apple', 1563, 2.35);
+//calcFruit('orange', 2500, 1.80);
+//calcFruit('apple', 1563, 2.35);
 
 //02. Greatest Common Divisor – GCD
 function greatestCommDivisor(input1, input2) {
@@ -19,23 +19,41 @@ function greatestCommDivisor(input1, input2) {
     }
     console.log(input1);
 }
-greatestCommDivisor(15, 5);
-greatestCommDivisor(2154, 458);
+//greatestCommDivisor(15, 5);
+//greatestCommDivisor(2154, 458);
 
 //03. Same Numbers
-function sameNumb(input) {
-    let isSame = false;
+function sameNumb(intNum) {
+    let input = intNum.toString();
+    let isSame = true;
+    let sum = parseInt(input[0]);
     for (let index = 1; index < input.length; index++) {
         let currentNumber = input[index];
         let previousNumber = input[index - 1];
-        if (previousNumber===currentNumber) {
-            isSame = true;
-        }else{
+        if (previousNumber === currentNumber) {
+        } else {
             isSame = false;
-            break;
         }
-            
+        sum += parseInt(input[index]);
     }
     console.log(isSame);
+    console.log(sum);
 }
-sameNumb(22222222222222);
+//sameNumb(2222222);
+//sameNumb(1234);
+
+//04. Previous Day
+function prevDay(year, month, day) {
+    // const date = new Date(year, month - 1, day);
+    // const resultDate = new Date();
+    //resultDate.setDate(date.getDate() - 1)
+    // console.log(resultDate);
+    let dateString = year + '-' + month + '-' + day;
+    let event = new Date(dateString);
+    event.setDate(day - 1);
+    console.log(event.getFullYear() + `-` + (Number(event.getMonth()) + 1) + '-' + event.getDate());
+}
+//prevDay(2016, 9, 30);
+//prevDay(2016, 10, 1);
+
+//05. Time to Walk
