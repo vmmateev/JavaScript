1c899b58e27ce310e833442eaec0d813740be9a8
vmmateev <v.m.mateev@gmail.com>
2021-09-17 22:06:56 +0300

    Exercise homework done!

--- 01.JavaScript Advanced/1.Exercise - Syntax, Functions and Statements/Exercise - Syntax, Functions and Statements.js
+++ 01.JavaScript Advanced/1.Exercise - Syntax, Functions and Statements/Exercise - Syntax, Functions and Statements.js
@@ -57,3 +57,124 @@ function prevDay(year, month, day) {
 //prevDay(2016, 10, 1);
 
 //05. Time to Walk
+function timeToWalk(stepsCount, meters, speed) {
+    let distance = stepsCount * meters;
+    let speedInMs = speed * 0.277778;
+    let time = distance / speedInMs;
+
+    let rest = Math.floor(distance / 500);
+
+    let timeMin = Math.floor(time / 60);
+    let timeSec = Math.round(time - (timeMin * 60));
+    let timeHr = Math.floor(time / 3600);
+
+    console.log((timeHr < 10 ? "0" : "") + timeHr + ":" + (timeMin + rest < 10 ? "0" : "") + (timeMin + rest) + ":" + (timeSec < 10 ? "0" : "") + timeSec);
+}
+// timeToWalk(4000, 0.60, 5);
+// timeToWalk(2564, 0.70, 5.5);
+//https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
+//6.Road Radar
+function roadRadar(speed, zone) {
+    let speedLimit;
+    switch (zone) {
+        case 'motorway': speedLimit = 130; break;
+        case 'interstate': speedLimit = 90; break;
+        case 'city': speedLimit = 50; break;
+        case 'residential': speedLimit = 20; break;
+    }
+
+    let speedDiff = Math.abs(speed - speedLimit);
+    let status = '';
+    let isSpeeded = true;
+    if (speed <= speedLimit) {
+        console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
+        isSpeeded = false;
+    } else {
+        if (speedDiff > 40) {
+            status = 'reckless driving';
+        } else if (speedDiff > 20 && speedDiff <= 40) {
+            status = 'excessive speeding';
+        } else if (speedDiff <= 20) {
+            status = 'speeding';
+        }
+        console.log(`The speed is ${speedDiff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+    }
+}
+// roadRadar(40, 'city');
+// roadRadar(21, 'residential');
+// roadRadar(120, 'interstate');
+// roadRadar(200, 'motorway');
+
+//07. Cooking by Numbers
+function cookNumbers(num, op1, op2, op3, op4, op5) {
+    let number = Number(num);
+
+    let arr = [op1, op2, op3, op4, op5];
+    for (let i = 0; i < arr.length; i++) {
+
+        switch (arr[i]) {
+            case 'chop': number = number / 2; break;
+            case 'dice': number = Math.sqrt(number); break;
+            case 'spice': number = number + 1; break;
+            case 'bake': number = number * 3; break;
+            case 'fillet': number = number - (number * 0.2); break;
+        }
+        console.log(number);
+    }
+}
+// cookNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+// cookNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
+
+//8.Validity Checker
+function validCheck(x1, y1, x2, y2) {
+
+    function distance(x1, y1, x2, y2) {
+        let distH = x1 - x2;
+        let distY = y1 - y2;
+        return Math.sqrt(distH ** 2 + distY ** 2);
+    }
+
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+// validCheck(3, 0, 0, 4);
+// validCheck(2, 1, 1, 1);
+
+//09.Words Uppercase (not included in final score)
+function toUpperFilter(text) {
+
+    let result = text.toUpperCase()
+        .split(/[\W]+/)
+        .filter(w => w.length > 0)
+        .join(", ");
+
+    console.log(result);
+}
+//   toUpperFilter('Hi, how are you?');
+//   toUpperFilter('hello');
+
+function toUpperMatch(text) {
+    let result = text.toUpperCase()
+        .match(/\w+/g)
+        .join(', ');
+
+    console.log(result);
+}
+//   toUpperMatch('Hi, how are you?');
+//   toUpperMatch('hello');
