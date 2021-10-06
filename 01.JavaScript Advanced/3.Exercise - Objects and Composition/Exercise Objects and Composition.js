cbc046c85f10c285b0c5afc66f82b9a2a7387b55
vmmateev <v.m.mateev@gmail.com>
2021-10-06 22:00:49 +0300

    Exercise Objects and Composition.js

--- 01.JavaScript Advanced/3.Exercise - Objects and Composition/Exercise Objects and Composition.js
+++ 01.JavaScript Advanced/3.Exercise - Objects and Composition/Exercise Objects and Composition.js
@@ -101,11 +101,110 @@ function heroicJson(strArr) {
     return JSON.stringify(result);
 }
 
-console.log(heroicJson(['Isacc / 25 / Apple, GravityGun',
-    'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara']
-));
-console.log(heroicJson(['Jake / 1000 / Gauss, HolidayGrenade']));
+// console.log(heroicJson(['Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara']
+// ));
+// console.log(heroicJson(['Jake / 1000 / Gauss, HolidayGrenade']));
 
 //5.Lowest Prices in Cities
+function lowestPrice(strArr) {
+    let catalogue = {};
 
+    strArr.forEach((el) => {
+        let [town, product, price] = el.split(' | ');
+        price = Number(price);
+
+        if (!catalogue[product]) {
+            catalogue[product] = {};
+        }
+
+        catalogue[product][town] = price;
+    });
+    for (const prod in catalogue) {
+        const sorted = Object.entries(catalogue[prod]).sort((a, b) => a[1] - b[1]);
+
+        console.log(`${prod} -> ${sorted[0][1]} (${sorted[0][0]})`);
+    }
+}
+
+// lowestPrice(['Sample Town | Sample Product | 1000',
+//     'Sample Town | Orange | 2',
+//     'Sample Town | Peach | 1',
+//     'Sofia | Orange | 3',
+//     'Sofia | Peach | 2',
+//     'New York | Sample Product | 1000.1',
+//     'New York | Burger | 10']
+// );
+
+//6.Store Catalogue
+function storeCatalogue(strArr) {
+    const productCatalogue = {};
+
+    strArr.forEach((el) => {
+        let [productName, price] = el.split(' : ');
+        price = Number(price);
+        const index = productName[0];
+        if (!productCatalogue[index]) {
+            productCatalogue[index] = {};
+        }
+        productCatalogue[index][productName] = price;
+
+    })
+    let intialSort = Object.keys(productCatalogue).sort((a, b) => a.localeCompare(b));
+
+
+    for (const key of intialSort) {
+        let products = Object.entries(productCatalogue[key]).sort((a, b) => a[0].localeCompare(b[0]));
+        console.log(key);
+        products.forEach((el) => {
+            console.log(`  ${el[0]}: ${el[1]}`);
+        })
+    }
+}
+
+// storeCatalogue(['Appricot : 20.4',
+//     'Fridge : 1500',
+//     'TV : 1499',
+//     'Deodorant : 10',
+//     'Boiler : 300',
+//     'Apple : 1.25',
+//     'Anti-Bug Spray : 15',
+//     'T-Shirt : 10']
+// );
+// storeCatalogue(['Banana : 2',
+//     'Rubic\'s Cube : 5',
+//     'Raspberry P : 4999',
+//     'Rolex : 100000',
+//     'Rollon : 10',
+//     'Rali Car : 2000000',
+//     'Pesho : 0.000001',
+//     'Barrel : 10']
+// );
+
+//7.Towns to JSON
+function townsToJson(strArr) {
+    const result = [];
+    const splitted = strArr[0].split('|');
+    const town = splitted[1].trim();
+    const latitude = splitted[2].trim();
+    const longitude = splitted[3].trim();
+
+    for (let i = 1; i < strArr.length; i++) {
+        const obj = {};
+        let splittedEntry = strArr[i].split('|');
+        
+        obj[town] = splittedEntry[1].trim();
+        obj[latitude] = Number(Number(splittedEntry[2].trim()).toFixed(2));
+        obj[longitude] = Number(Number(splittedEntry[3].trim()).toFixed(2));
+
+        result.push(obj);
+    }
+
+    return JSON.stringify(result);
+}
+
+console.log(townsToJson(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |']
+));
